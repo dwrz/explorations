e8d5400d85a258bb856f8595ae92741148bdfa82
dwrz <dwrz@dwrz.net>
2018-04-04 03:09:14 +0000

    Add concurrent.js
    Explore concurrent function invocations. This is to see if I can use
    concurrency to speed up backend queries that must make multiple db
    calls, then combine and process them into a single response.

---
+++ js/async/concurrent.js
@@ -0,0 +1,26 @@
+const delay = (time = 1000) => new Promise((resolve) => {
+  setTimeout(resolve.bind(null, true), time);
+});
+
+const delay2seconds = delay.bind(null, 2000);
+const delay3seconds = delay.bind(null, 3000);
+const delay5seconds = delay.bind(null, 5000);
+
+const sumDelays = (...delays) =>
+  delays.reduce((promise, f) => promise
+    .then(f), Promise.resolve());
+
+const Α = () => sumDelays(delay2seconds, delay3seconds)
+  .then(() => 'I AM THE Α.');
+
+const Ω = () => delay5seconds()
+  .then(() => 'I AM THE Ω.');
+
+const executeConcurrently = (...functions) =>
+  Promise.all(functions.map(f => f()));
+
+executeConcurrently(Α, Ω)
+  .then(arr => console.log(JSON.stringify(arr)))
+  .then(() => {
+    process.exit(0);
+  });
