30f503323d5020d003724a78b78f85ec918739c8
dwrz <dwrz@dwrz.net>
2018-04-16 01:53:11 +0000

    Add handling of no input args

--- go/ageInDays.go
+++ go/ageInDays.go
@@ -8,10 +8,15 @@ import (
 )
 
 func main() {
+	if len(os.Args) <= 1 {
+		fmt.Println("PLEASE ENTER A DATE IN YYYYMMDD FORMAT.")
+		fmt.Printf("EG: %s FOR TODAY'S DATE.", time.Now().Format("20060102"))
+		os.Exit(1)
+	}
 	dob, err := time.Parse("20060102", os.Args[1])
 	if err != nil {
 		fmt.Println("UNABLE TO PARSE INPUTTED DATE.")
-		os.Exit(1)
+		os.Exit(2)
 	}
 	fmt.Printf("YOU ARE ~%d DAYS OLD.", calcElapsedDays(dob))
 	return
