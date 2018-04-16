adeeb7eb0274ce907142d6261487657ec3396c17
dwrz <dwrz@dwrz.net>
2018-04-16 02:00:01 +0000

    Refactor to separate function

--- go/ageInDays.go
+++ go/ageInDays.go
@@ -13,6 +13,10 @@ func main() {
 		fmt.Printf("EG: %s FOR TODAY'S DATE.", time.Now().Format("20060102"))
 		os.Exit(1)
 	}
+	calcAgeInDays()
+}
+
+func calcAgeInDays () {
 	dob, err := time.Parse("20060102", os.Args[1])
 	if err != nil {
 		fmt.Println("UNABLE TO PARSE INPUTTED DATE.")
