e273f6bfeee83c3dc0320edefbc6972803e9439d
dwrz <dwrz@dwrz.net>
2018-04-16 02:42:57 +0000

    Document ageInDays

--- go/ageInDays.go
+++ go/ageInDays.go
@@ -1,3 +1,11 @@
+// Calculate days elapsed (age) from an inputted date to the present moment.
+//
+// The days of our years are threescore years and ten;
+// and if by reason of strength they be fourscore years,
+// yet is their strength labour and sorrow;
+// for it is soon cut off, and we fly away.
+// Psalm 90:10
+
 package main
 
 import (
